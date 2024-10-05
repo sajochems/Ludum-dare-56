d4d20fe0b0ffe58165d27ccf49856c2769cbf953
Scott Jochems <s.a.jochems@student.tudelft.nl>
2024-10-05 12:38:17 +0200

    Added simple tilemap and player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public PlayerInputActions playerControls;
+
+    private Vector2 moveDirection = Vector2.zero;
+    private InputAction move;
+    private InputAction fire;
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+    }
+
+    private void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Debug.Log("We Fired at: (" + mousePos.x + ", " + mousePos.y + ")");
+    }
+}
