66bad05c08d9271aa36d7dfb6fc4ac999aedb11b
sajochems <scottiej2001@gmail.com>
2024-10-07 17:23:32 +0200

    Added a volume slider and balanced sounds

---
+++ Assets/Scripts/FireCat.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCat : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 2f;
+    public int bulletDamage = 100;
+
+    public float attackSpeed = 200f;
+
+    public float speed = 2f;
+
+    private float counter;
+
+    private Queue<Enemy> targets;
+    private Transform player;
+
+    void Start()
+    {
+        targets = new Queue<Enemy>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= 100 / attackSpeed)
+        {
+            if (targets.Count > 0)
+            {
+                Enemy target = targets.Peek();
+                if (target == null)
+                {
+                    targets.Dequeue();
+                }
+                else
+                {
+                    Attack(target);
+                    counter = 0;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 playerDirection = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+        rb.velocity = new Vector2(playerDirection.x * speed, playerDirection.y * speed);
+    }
+
+    private void Attack(Enemy target)
+    {
+        Transform targetLocation = target.GetTransform();
+        Vector3 rotation = targetLocation.position - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        Vector3 bulletDirection = targetLocation.position - bulletSpawnPoint.position;
+
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.Euler(0, 0, rotZ));
+        bullet.GetComponent<Bullet>().SetDamage(bulletDamage);
+        bullet.GetComponent<Rigidbody2D>().velocity = bulletDirection * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            targets.Enqueue(collision.gameObject.GetComponent<Enemy>());
+        }
+    }
+}
---
+++ Assets/Scripts/GunCatShop.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunCatShop : MonoBehaviour
+{
+    public GameObject gunCatPrefab;
+
+    public GameObject canvas;
+
+    private void Start()
+    {
+        canvas.SetActive(false);
+    }
+    public void BuyCat()
+    {
+        GameState.DecreaseCatfood(50);
+        Instantiate(gunCatPrefab, transform.position, transform.rotation);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            canvas.SetActive(true);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
